5b718d660f38548a8d4d72e1309488129b217a19
lilian crascall-kennedy <lcrascal@glx-calcul3.ias.u-psud.fr>
2025-09-09 17:41:59 +0200

    added function to create a bubble field with thermal noise. Noise parameters (T_sys, bandwidth, integration_time, num_antennas)

--- Create_Bubble_Fields_Functions.py
+++ Create_Bubble_Fields_Functions.py
@@ -126,5 +126,80 @@ plt.imshow(cube.box)
 
 
 ###################################################################################################################################################
-##############################  ###################################################
+############################## Creat Bubble Field with Thermal Noise ##############################################################################
 ###################################################################################################################################################
+
+def create_bubble_field_with_thermal_noise(
+    DIM, ff, radius, sigma, NDIM, nooverlap, periodic, radius_distribution, gaussian_profile,
+    T_sys, bandwidth, integration_time, num_antennas, save=True, save_dir="."):
+    """
+    Create a clean RandomBubbles-Extended field, add thermal noise, and (optionally) save.
+
+    Returns
+    -------
+    filename_stub : str
+    field_noisy : np.ndarray
+    cube : object         # the RB_extended cube with .box set to the noisy field
+    """
+    # --- generate the clean field  ---
+    # NOTE: requires Create_Bubble_Field_Func_Extended to accept `save=False`
+    _, cube = Create_Bubble_Field_Func_Extended(
+        DIM, ff, radius, sigma, NDIM, nooverlap, periodic,
+        radius_distribution, gaussian_profile,
+        verbose=False, save=False, show_plot=False
+    )
+    field = cube.box
+
+    # --- sanity checks for noise params ---
+    if None in (T_sys, bandwidth, integration_time, num_antennas):
+        raise ValueError(
+            "Provide T_sys, bandwidth, integration_time and num_antennas.")
+
+
+    # --- noise draw ---
+    sigma_noise = T_sys / np.sqrt(bandwidth * integration_time * num_antennas)
+    thermal_noise = np.random.normal(loc=0, scale=sigma_noise, size=data.shape)
+    field_noisy = (field + thermal_noise)
+
+    # keep cube consistent for downstream users
+    if hasattr(cube, "box"):
+        cube.box = field_noisy
+
+    # --- filename + save ---
+    filename_stub = (
+        f"Field_NOISY"
+        f"_FF{ff:.3f}_R{radius}_S{sigma}"
+        f"_N{NDIM}_DIM{DIM}"
+        f"_noov{int(bool(nooverlap))}_per{int(bool(periodic))}"
+        f"_RD{radius_distribution}_GP{int(bool(gaussian_profile))}"
+    )
+    if save:
+        np.savetxt(os.path.join(save_dir, filename_stub + ".txt"), field_noisy)
+
+    return filename_stub, field_noisy, cube
+
+
+# Example Usage
+
+DIM = 200
+ff = 0.05
+radius = 5
+ff = 0.01
+sigma = 2
+NDIM = 2
+nooverlap = False
+periodic = False
+radius_distribution = 1
+gaussian_profile = False
+T_sys = 100
+bandwidth = 100
+integration_time = 600
+num_antennas = 128
+
+
+filename_stub, field_noisy, cube = create_bubble_field_with_thermal_noise(DIM, ff, radius, sigma, NDIM, nooverlap, periodic, radius_distribution, gaussian_profile,
+                                       T_sys, bandwidth, integration_time, num_antennas, save=True, save_dir=".")
+
+print(filename_stub )
+print(np.shape(field_noisy))
+plt.imshow(cube.box)
