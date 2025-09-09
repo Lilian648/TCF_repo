b3deff75af3e1fb550de452955f2a270dbafca96
lilian crascall-kennedy <lcrascal@glx-calcul3.ias.u-psud.fr>
2025-09-09 14:32:39 +0200

    file contains simplest function to create a bubble field using Random_bubbles.py (with args DIM, NDIM, periodic, nooverlap, radius, ff) and example usage

---
+++ Create_Bubble_Fields_Functions.py
@@ -0,0 +1,48 @@
+import subprocess
+import pandas as pd
+import os
+import re
+import scipy.stats as stats
+import glob, pickle
+import numpy as np
+from scipy.fft import fft2, ifft2, fftshift, ifftshift
+from Random_bubbles import *
+
+
+### Function to create a bubble field 
+
+def Create_Bubble_Field_Func(DIM, ff, radius, NDIM, nooverlap, periodic):
+    """
+    Generates a model field and returns the filename and a data cube
+    cube.box = 2/3D data array
+    """
+
+    # create field
+    cube = RandomBubbles(DIM, ff, radius, NDIM, nooverlap=False, periodic=True)
+
+    # save field to txt file
+    RandomBubbles.write_ionisation_field(cube) 
+
+    # get filename
+    filename = RandomBubbles.print_filechain(cube)
+    
+    return filename, cube
+
+
+# Example usage
+
+DIM = 200
+ff = 0.05
+radius = 5
+NDIM = 2
+nooverlap = False
+periodic = False
+
+
+filename, cube = Create_Bubble_Field_Func(DIM, ff, radius, NDIM, nooverlap, periodic)
+
+data = np.loadtxt(filename + '.txt')
+
+plt.imshow(cube.box)
+plt.imshow(data) # these two imshows are equivalent
+
